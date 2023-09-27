9126c32a5d782328d779d34a0cf634c859452039
SpidyRahim <mdrahim5974@gmail.com>
2023-09-27 10:08:55 +0600

    Lab

--- secant_method.py
+++ secant_method.py
@@ -37,3 +37,37 @@ def secant(a, b, e, N):
 
 # Starting Secant Method
 secant(0, 1, 0.000048, 3)
+
+
+# Defining Function
+# def f(x):
+#     return 2.718281**(-x) - x
+
+# # Implementing Secant Method
+# def secant(a, b, e, N):
+#     print('\n\n*** SECANT METHOD IMPLEMENTATION ***')
+#     step = 1
+#     condition = True
+#     if f(a) * f(b) >= 0:
+#         print('Initial guesses do not bracket the root.')
+#         return
+#     while condition:
+#         c = a - (b-a)*f(a)/(f(b) - f(a))
+#         print(f"Iteration= {step}, c = {c:.6f} and f(c) = {f(c):.6f}")
+#         a = b
+#         b = c
+#         step = step + 1
+#         if step > N:
+#             print('Not Convergent!')
+#             break
+#         condition = abs(f(c)) > e
+#     print('\n Required root is: %0.8f' % c)
+
+# # Input Section
+# a = float(input('Enter First Guess: '))
+# b = float(input('Enter Second Guess: '))
+# e = float(input('Tolerable Error: '))
+# N = int(input('Maximum Step: '))
+
+# # Starting Secant Method
+# secant(a, b, e, N)
